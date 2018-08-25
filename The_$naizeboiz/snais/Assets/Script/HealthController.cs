d3db32398d971329e07e2d833e3501eec13a26f6
chrkol <colding93@gmail.com>
2018-08-25 19:32:49 +0200

    BIG SVAIUPD
    Added:
    
    DMG,
    RELOAD,
    HEALTH,
    TREEEES,
    AUTO OG SEMI BØRST
    
    MINDRE BØG FIXES

---
+++ The_$naizeboiz/snais/Assets/Script/HealthController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthController : MonoBehaviour
+{
+
+    [SerializeField] private float health = 100f;
+
+
+    public void ApplyDOMAGE(float domage)
+    {
+        // Debug.Log("You fucked up son: " + domage);
+        health -= domage;
+
+        if(health <= 0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
--- The_$naizeboiz/snais/Assets/Script/Wepon/Wepon.cs
+++ The_$naizeboiz/snais/Assets/Script/Wepon/Wepon.cs
@@ -16,57 +16,145 @@ public class Wepon : MonoBehaviour {
     private AudioSource _AudioSource;
     public AudioClip shootSound;
     public ParticleSystem muzzleFlash;
+    private bool isReloading;
+    private bool shootInput;
+    public float domage = 20f;
+
+    public enum ShootMode { Auto, Semi}
+    public ShootMode shootingMode;
+
+
+    private Vector3 originalPosition;
+    public Vector3 aimPosition;
+    public float aodSpeed;
+
+    [Header("WepansConfig")]
+    public GameObject hitParticles;
+    public GameObject bulletImapt;
     
 	// Use this for initialization
 	void Start () {
         anim = GetComponent<Animator>();
         _AudioSource = GetComponent<AudioSource>();
         currentBullets = bulletsPerMag;
+
+        originalPosition = transform.localPosition;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetButton("Fire1"))
+
+        switch(shootingMode)
         {
-            Fire(); // exec hvis du press elle hold knappen
+            case ShootMode.Auto:
+                shootInput = Input.GetButton("Fire1");
+            break;
+
+            case ShootMode.Semi:
+                shootInput = Input.GetButtonDown("Fire1");
+            break;
+        }
+        if (shootInput)
+        {
+            if (currentBullets > 0)
+                Fire(); // exec hvis du press elle hold knappen
+            else if(bulletsLeft > 0)
+                DoReload();
         }
 
         if (fireTimer < fireRate)
             fireTimer += Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if(currentBullets < bulletsPerMag && bulletsLeft > 0)
+            DoReload();
+        }
+
+        AimDownSights();
 	}
 
     void FixedUpdate()
     {
         AnimatorStateInfo info = anim.GetCurrentAnimatorStateInfo(0);
-
-        if (info.IsName("Fire")) anim.SetBool("Fire", false);
+        isReloading = info.IsName("Reload");
+        //if (info.IsName("Fire")) anim.SetBool("Fire", false);
     }
 
-
+    private void AimDownSights()
+    {
+        if(Input.GetButton("Fire2") && !isReloading)
+        {
+            transform.localPosition = Vector3.Lerp(transform.localPosition, aimPosition, Time.deltaTime * aodSpeed);
+        }
+        else
+        {
+            transform.localPosition = Vector3.Lerp(transform.localPosition, originalPosition, Time.deltaTime * aodSpeed);
+        }
+    }
 
 
     private void Fire()
     {
-        if (fireTimer < fireRate || currentBullets <= 0) return; 
+        if (fireTimer < fireRate || currentBullets <= 0 || isReloading)
+            return; 
 
         RaycastHit hit;
         if(Physics.Raycast(shootpoint.position, shootpoint.forward, out hit, weaponRange))
         {
             Debug.Log(hit.transform.name + " found ");
+            GameObject hitParticleEffect = Instantiate(hitParticles, hit.point, Quaternion.FromToRotation(Vector3.up, hit.normal));
+           // hitParticleEffect.transform.SetParent(hit.transform);
+            GameObject bulletHole = Instantiate(bulletImapt, hit.point, Quaternion.FromToRotation(Vector3.forward, hit.normal));
+
+            Destroy(hitParticleEffect, 1f);
+            Destroy(bulletHole, 1f);
+
+            if(hit.transform.GetComponent<HealthController>())
+            {
+                hit.transform.GetComponent<HealthController>().ApplyDOMAGE(domage);
+            }
         }
 
 
-        anim.SetBool("Fire", true);
-        muzzleFlash.Play();
-        PlayShootSound();
+        anim.CrossFadeInFixedTime("Fire", 0.01f); //Plays the fire animation
+        
         currentBullets--;
         fireTimer = 0.0f; //resetter fireTimer
+        PlayShootSound(); //Spiller av soundclip
+        muzzleFlash.Play();
+
 
     }
 
+    public void Reload()
+    {
+        if (bulletsLeft <= 0) return;
+
+        int bulletsToLoad = bulletsPerMag-currentBullets; //Sjekker hvor mange kuler som addes til maget
+
+        //                                IF                then      1st   else    2nd
+        int bulletsToDeduct = (bulletsLeft >= bulletsToLoad) ? bulletsToLoad : bulletsLeft;
+
+        bulletsLeft -= bulletsToDeduct;
+        currentBullets += bulletsToDeduct;
+
+    }
+
+    private void DoReload()
+    {
+        AnimatorStateInfo info = anim.GetCurrentAnimatorStateInfo(0);
+
+        if (isReloading) return;
+        
+        anim.CrossFadeInFixedTime("Reload", 0.01f);
+    }
+
+
     private void PlayShootSound()
     {
-        _AudioSource.clip = shootSound;
-        _AudioSource.Play();
+        _AudioSource.PlayOneShot(shootSound);
+        //_AudioSource.clip = shootSound;
+        //_AudioSource.Play();
     }
 }
